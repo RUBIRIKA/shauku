61c145a7082fdeafd840a439000eaab86915c397
RUBIRIKA <emmanu.ru1994@gmail.com>
2023-04-18 15:59:08 +0200

    arsenal

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -3,7 +3,7 @@ import Image from 'next/legacy/image';
 
 function Grid() {
   return (
-    <div className=" mt-12 px-8 pb-10 mx-auto mb-10 gap-2 columns-4 space-y-2 md:space-y-3 md:gap-3 lg:space-y-3 lg:gap-3 xl:space-y-5  xl:gap-5 2xl:space-y-5  2xl:gap-5  ">
+    <div className=" mt-12 px-8 pb-10 mx-auto mb-10 gap-2 columns-2 md:columns-3 lg:columns-4 xl:columns-4 space-y-2 md:space-y-3 md:gap-3 lg:space-y-3 lg:gap-3 xl:space-y-5  xl:gap-5 2xl:space-y-5  2xl:gap-5  ">
         
           
            <img src="/images/b2.jpg" className="cursor-pointer rounded " alt="" />
